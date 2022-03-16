830306f765b10c9929c9809234d4079273a5bf68
rafybot <rafaelagasaisan@gmail.com>
2022-03-16 11:08:57 -0300

    ✨ using get & params + nodemon

--- controller/routes/index.js
+++ controller/routes/index.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", function(req, resp) {
+    resp.send("Bem-vindo ao app!")
+});
 
+app.get("/contato", function(req, resp) {
+    resp.send("Página de <b>contato</b> do app.")
+});
+
+app.get("/contato/:nome/:cargo", function(req, resp) {
+    resp.send("<h1>Olá sr(a). <b>"+ req.params.nome +"</b>.</h1>    <h2>Seu cargo é " + req.params.cargo + "</h2>")
+})
 
 
 //esta deve ser sempre a última linha ao usar o express
