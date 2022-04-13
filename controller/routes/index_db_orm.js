860b98fc3cd0c75e1ddcae42bee1082b63e0b3bc
rafybot <rafaelagasaisan@gmail.com>
2022-04-13 11:03:06 -0300

    ✨ insert in orm

--- controller/routes/index_db_orm.js
+++ controller/routes/index_db_orm.js
@@ -5,4 +5,12 @@
     console.log('==================== Criar tabela ====================')
     const resultado = await database.sequelize.sync();
     console.log(resultado);
+
+    console.log('==================== Criar registro ====================')
+    const inserirCliente = await Cliente.create({
+        nome: 'João',
+        idade: 18,
+        endereco: 'Rua Paulista, n 100'
+    })
+    console.log(inserirCliente);
 })();
